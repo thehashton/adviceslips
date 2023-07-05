39283583c603b362a23ecb270586306ebe8b8607
Harry Ashton <harry.ashton.uk@gmail.com>
2023-07-05 22:30:10 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import reactLogo from './assets/react.svg'
-import tsLogo from './assets/ts-logo.svg'
-import sassLogo from './assets/sass.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Counter from "./components/Counter/Counter";
 
 function App() {
   return (
